88314c7845ee9cd29123704db28da7bd5ee04e4b
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-04 16:54:34 +0900

    [FIX] デバック用の標準出力を削除

--- src/main/java/io/github/kuri_megane/Controller.java
+++ src/main/java/io/github/kuri_megane/Controller.java
@@ -18,51 +18,33 @@ public class Controller implements NativeKeyListener {
     // キーが押された
     public void nativeKeyPressed(NativeKeyEvent e) {
 
+        // 左矢印キー
+        if (e.getRawCode() == 123) {
+            if (gameMap.get(pacMan.getRow(), pacMan.getCol() - 1).isGo()) {
+                gameMap.move(pacMan, new Point(pacMan.getRow(), pacMan.getCol() - 1));
+            }
+        }
+
         // 右矢印キー
         if (e.getRawCode() == 124) {
-
             if (gameMap.get(pacMan.getRow(), pacMan.getCol() + 1).isGo()) {
                 gameMap.move(pacMan, new Point(pacMan.getRow(), pacMan.getCol() + 1));
             }
-
-            System.out.println("右");
-
-        }
-
-        // 左矢印キー
-        if (e.getRawCode() == 123) {
-
-           if (gameMap.get(pacMan.getRow(), pacMan.getCol() - 1).isGo()) {
-                gameMap.move(pacMan, new Point(pacMan.getRow(), pacMan.getCol() - 1));
-            }
-
-            System.out.println("左");
-
         }
 
         // 下矢印キー
         if (e.getRawCode() == 125) {
-
             if (gameMap.get(pacMan.getRow() + 1, pacMan.getCol()).isGo()) {
                 gameMap.move(pacMan, new Point(pacMan.getRow() + 1, pacMan.getCol()));
             }
-
-            System.out.println("下");
-
         }
 
         // 上矢印キー
         if (e.getRawCode() == 126) {
-
             if (gameMap.get(pacMan.getRow() - 1, pacMan.getCol()).isGo()) {
                 gameMap.move(pacMan, new Point(pacMan.getRow() - 1, pacMan.getCol()));
             }
-
-            System.out.println("上");
-
         }
-
-        System.out.println(e.paramString());
     }
 
     // キーが離された
