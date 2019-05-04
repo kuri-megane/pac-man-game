d63b9577285f03c2d345dc687e3f0ae9c33f0a4c
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-04 19:07:15 +0900

    [ADD] スタートメニューを表示

--- src/main/java/io/github/kuri_megane/Layer/StartMenu.java
+++ src/main/java/io/github/kuri_megane/Layer/StartMenu.java
@@ -1,4 +1,70 @@
 package io.github.kuri_megane.Layer;
 
+import jline.Terminal;
+import jline.TerminalFactory;
+import jline.console.ConsoleReader;
+
+import java.io.IOException;
+
 public class StartMenu implements BackLayer {
+
+    private int consoleHeight = 0;
+    private int consoleWidth = 0;
+    private ConsoleReader console;
+
+    // XXX: ここにベタ書きするの良くない
+    private static final String strDisplay =
+              " ____            __  __              ____                      %n"
+            + "|  _ \\ __ _  ___|  \\/  | __ _ _ __  / ___| __ _ _ __ ___   ___ %n"
+            + "| |_) / _` |/ __| |\\/| |/ _` | '_ \\| |  _ / _` | '_ ` _ \\ / _ \\%n"
+            + "|  __/ (_| | (__| |  | | (_| | | | | |_| | (_| | | | | | |  __/%n"
+            + "|_|   \\__,_|\\___|_|  |_|\\__,_|_| |_|\\____|\\__,_|_| |_| |_|\\___|%n"
+            + "%n"
+            + "                           Ready...?                                %n";
+
+    public StartMenu(){
+        try {
+            console = new ConsoleReader();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * コンソールサイズを取得します．
+     */
+    public void getConsoleSize(){
+        Terminal terminal = TerminalFactory.get();
+        consoleHeight = terminal.getHeight();
+        consoleWidth = terminal.getWidth();
+    }
+
+    /**
+     * スタートメニューの描画
+     *
+     * @param strings カスタムで先に表示する文字列
+     */
+    public void display(String strings){
+
+        // クリア
+        try {
+            console.clearScreen();
+            console.flush();
+        }
+        catch (IOException e){
+            e.getMessage();
+        }
+
+        // 表示
+        System.out.println(strings);
+        System.out.printf(strDisplay);
+
+        // 一時スリープ
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/io/github/kuri_megane/Main.java
+++ src/main/java/io/github/kuri_megane/Main.java
@@ -1,7 +1,13 @@
 package io.github.kuri_megane;
 
+import io.github.kuri_megane.Layer.StartMenu;
+
 public class Main {
     public static void main(String[] args) {
+
+        StartMenu startMenu = new StartMenu();
+        startMenu.display("");
+
         Game game = new Game();
         game.run();
     }
