7304399d3dab8e9c81ee6060cefe96abc565b42a
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-05 02:43:36 +0900

    [FIX] Monster と衝突したときの表示を変更

--- src/main/java/io/github/kuri_megane/Objects/Ban.java
+++ src/main/java/io/github/kuri_megane/Objects/Ban.java
@@ -1,8 +1,11 @@
 package io.github.kuri_megane.Objects;
 
+import io.github.kuri_megane.Objects.Color.Red;
+
 public class Ban implements GameObjects {
 
-    private static final String displayChar = "💥";
+    private static final Red red = new Red(" ");
+    private static final String displayChar = red.coloringBack();
     private static final boolean goFlag = true;
     private static final String key = "w";
 
