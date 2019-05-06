fdb216f5cedebfadb7c7dc1bc8d6b31e8da26238
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-07 01:21:57 +0900

    [ADD] インタフェースのコメントを追加

--- src/main/java/io/github/kuri_megane/Objects/GameObjects.java
+++ src/main/java/io/github/kuri_megane/Objects/GameObjects.java
@@ -1,9 +1,25 @@
 package io.github.kuri_megane.Objects;
 
 public interface GameObjects {
+
+    /**
+     * 当該オブジェクトをどのように表示させるか取得します．
+     *
+     * @return 表示用の文字 ex. PacMan:"●"
+     */
     String getChar();
 
+    /**
+     * 当該オブジェクトのある位置に移動できるか取得します．
+     *
+     * @return 移動できるのであれば true
+     */
     boolean isGo();
 
+    /**
+     * 当該オブジェクトを示すキーを取得します．
+     *
+     * @return 文字列 ex. PacMan:"p"
+     */
     String getKey();
 }
--- src/main/java/io/github/kuri_megane/Objects/MovingObjects.java
+++ src/main/java/io/github/kuri_megane/Objects/MovingObjects.java
@@ -2,20 +2,55 @@ package io.github.kuri_megane.Objects;
 
 public interface MovingObjects {
 
+    /**
+     * どの行か取得します．
+     *
+     * @return 行の数字
+     */
     int getRow();
 
-    int getCol();
-
+    /**
+     * どの行にいるか設定します．
+     *
+     * @param row 行の数字
+     */
     void setRow(int row);
 
+    /**
+     * どの列か取得します．
+     *
+     * @return 列の数字
+     */
+    int getCol();
+
+    /**
+     * どの列にいるか設定します．
+     *
+     * @param col 列の筋
+     */
     void setCol(int col);
 
+    /**
+     * 直前にどの座標にいたか設定します．
+     */
     void setBeforePoint();
 
+    /**
+     * スコアを加算します．
+     * 獲得した(通過した) Cookie の数です．
+     */
     void addScore();
 
+    /**
+     * スコアを取得します．
+     *
+     * @return スコアの値
+     */
     int getScore();
 
+    /**
+     * 移動に失敗したときの挙動を定義します．
+     */
     void failToMove();
 
 }
