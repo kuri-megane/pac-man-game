70e858c12825264258c92552291223ad271af662
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-06 12:32:55 +0900

    [FIX] プログラムの終了を system.exit() に変更

--- src/main/java/io/github/kuri_megane/Main.java
+++ src/main/java/io/github/kuri_megane/Main.java
@@ -26,43 +26,64 @@ public class Main {
         game1.tearDown();
         gamaData.setPlayerScore(game1.getPlayerScore() + gamaData.getPlayerScore());
         gamaData.setMonsterScore(game1.getMonsterScore() + gamaData.getMonsterScore());
+        gamaData.setDead(game1.isPlayerDead());
 
         // 結果画面表示
         EndRoll endRoll = new EndRoll();
         endRoll.getConsoleSize();
         endRoll.display(endRoll.createResultStrings(gamaData), 5000);
 
-        // 1ゲーム目をクリアしたら
-        if (gamaData.getMonsterScore() <= gamaData.getPlayerScore()) {
-
-            // 2ゲーム目
-            Game game2 = new Game();
-            game2.setUp();
-            game2.runMonster2();
-            game2.tearDown();
-            gamaData.setPlayerScore(game2.getPlayerScore() + gamaData.getPlayerScore());
-            gamaData.setMonsterScore(game2.getMonsterScore() + gamaData.getMonsterScore());
-
-            // 結果画面表示
-            endRoll.getConsoleSize();
-            endRoll.display(endRoll.createResultStrings(gamaData), 5000);
+        // 死んでたら
+        if (gamaData.isDead()) {
+            // return;
+            // XXX: スレッドがうまく終了できていないため
+            System.exit(0);
+        }
+        // 得点が低かったら
+        if (gamaData.getPlayerScore() <= gamaData.getMonsterScore()) {
+            // return;
+            // XXX: スレッドがうまく終了できていないため
+            System.exit(0);
         }
 
-        // 2ゲーム目をクリアしたら
-        if (gamaData.getMonsterScore() <= gamaData.getPlayerScore()) {
+        // 2ゲーム目
+        Game game2 = new Game();
+        game2.setUp();
+        game2.runMonster2();
+        game2.tearDown();
+        gamaData.setPlayerScore(game2.getPlayerScore() + gamaData.getPlayerScore());
+        gamaData.setMonsterScore(game2.getMonsterScore() + gamaData.getMonsterScore());
+        gamaData.setDead(game2.isPlayerDead());
 
-            // 3ゲーム目
-            Game game3 = new Game();
-            game3.setUp();
-            game3.runMonster4();
-            game3.tearDown();
-            gamaData.setPlayerScore(game3.getPlayerScore() + gamaData.getPlayerScore());
-            gamaData.setMonsterScore(game3.getMonsterScore() + gamaData.getMonsterScore());
+        // 結果画面表示
+        endRoll.getConsoleSize();
+        endRoll.display(endRoll.createResultStrings(gamaData), 5000);
 
-            // 結果画面表示
-            endRoll.getConsoleSize();
-            endRoll.display(endRoll.createResultStrings(gamaData), 5000);
+        // 死んでたら
+        if (gamaData.isDead()) {
+            // return;
+            // XXX: スレッドがうまく終了できていないため
+            System.exit(0);
         }
+        // 得点が低かったら
+        if (gamaData.getPlayerScore() <= gamaData.getMonsterScore()) {
+            // return;
+            // XXX: スレッドがうまく終了できていないため
+            System.exit(0);
+        }
+
+        // 3ゲーム目
+        Game game3 = new Game();
+        game3.setUp();
+        game3.runMonster4();
+        game3.tearDown();
+        gamaData.setPlayerScore(game3.getPlayerScore() + gamaData.getPlayerScore());
+        gamaData.setMonsterScore(game3.getMonsterScore() + gamaData.getMonsterScore());
+        gamaData.setDead(game3.isPlayerDead());
+
+        // 結果画面表示
+        endRoll.getConsoleSize();
+        endRoll.display(endRoll.createResultStrings(gamaData), 5000);
 
         // 終了
         // XXX: スレッドがうまく終了できていないため
