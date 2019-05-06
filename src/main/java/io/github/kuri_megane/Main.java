722c0311dbefbdd3e62a302ed2b8a3a2adf7400d
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-06 11:44:30 +0900

    [FIX] ゲームスコアがうまくはいっていなかったことを修正

--- src/main/java/io/github/kuri_megane/Main.java
+++ src/main/java/io/github/kuri_megane/Main.java
@@ -23,9 +23,9 @@ public class Main {
         Game game1 = new Game();
         game1.setUp();
         game1.runMonster1();
+        game1.tearDown();
         gamaData.setPlayerScore(game1.getPlayerScore() + gamaData.getPlayerScore());
         gamaData.setMonsterScore(game1.getMonsterScore() + gamaData.getMonsterScore());
-        game1.tearDown();
 
         // 結果画面表示
         EndRoll endRoll = new EndRoll();
@@ -39,9 +39,9 @@ public class Main {
             Game game2 = new Game();
             game2.setUp();
             game2.runMonster2();
+            game2.tearDown();
             gamaData.setPlayerScore(game2.getPlayerScore() + gamaData.getPlayerScore());
             gamaData.setMonsterScore(game2.getMonsterScore() + gamaData.getMonsterScore());
-            game2.tearDown();
 
             // 結果画面表示
             endRoll.getConsoleSize();
@@ -55,9 +55,9 @@ public class Main {
             Game game3 = new Game();
             game3.setUp();
             game3.runMonster4();
+            game3.tearDown();
             gamaData.setPlayerScore(game3.getPlayerScore() + gamaData.getPlayerScore());
             gamaData.setMonsterScore(game3.getMonsterScore() + gamaData.getMonsterScore());
-            game3.tearDown();
 
             // 結果画面表示
             endRoll.getConsoleSize();
