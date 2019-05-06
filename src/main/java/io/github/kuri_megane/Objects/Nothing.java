f6a5fae4cb33c50ec345e171db6ca5c79d6aeaba
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-06 20:39:29 +0900

    [ADD] Nothing のテストを追加，コメントを追加

--- src/main/java/io/github/kuri_megane/Objects/Nothing.java
+++ src/main/java/io/github/kuri_megane/Objects/Nothing.java
@@ -1,5 +1,8 @@
 package io.github.kuri_megane.Objects;
 
+/**
+ * Nothing で生成したインスタンスは何も無いことを示すオブジェクトです
+ */
 public class Nothing implements GameObjects {
 
     private static final String displayChar = " ";
@@ -9,14 +12,30 @@ public class Nothing implements GameObjects {
     public Nothing() {
     }
 
+    /**
+     * Nothing をどのように表示させるか取得します．
+     *
+     * @return 表示用の文字
+     */
     public String getChar() {
         return displayChar;
     }
 
+    /**
+     * この Nothing のある位置に移動できるか取得します．
+     * Nothing は true です．
+     *
+     * @return 移動できるかどうかのフラグ
+     */
     public boolean isGo() {
         return goFlag;
     }
 
+    /**
+     * Nothing であることを示すキーを取得します．
+     *
+     * @return 文字列 "n" (private static final String key で指定)
+     */
     public String getKey() {
         return key;
     }
--- src/test/java/io/github/kuri_megane/Objects/NothingTest.java
+++ src/test/java/io/github/kuri_megane/Objects/NothingTest.java
@@ -1,4 +1,27 @@
 package io.github.kuri_megane.Objects;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class NothingTest {
+
+    @Test
+    public void testGetChar() {
+        Nothing nothing = new Nothing();
+        assertThat(nothing.getChar(), is(" "));
+    }
+
+    @Test
+    public void testIsGo() {
+        Nothing nothing = new Nothing();
+        assertThat(nothing.isGo(), is(true));
+    }
+
+    @Test
+    public void testGetKey() {
+        Nothing nothing = new Nothing();
+        assertThat(nothing.getKey(), is("n"));
+    }
 }
