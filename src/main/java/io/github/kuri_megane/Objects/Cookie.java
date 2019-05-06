b52d794ba948fe21cd205ef421443405ca444335
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2019-05-06 20:36:11 +0900

    [ADD] Cookie のテストを追加，コメントを追加

--- src/main/java/io/github/kuri_megane/Objects/Cookie.java
+++ src/main/java/io/github/kuri_megane/Objects/Cookie.java
@@ -9,14 +9,30 @@ public class Cookie implements GameObjects {
     public Cookie() {
     }
 
+    /**
+     * Cookie をどのように表示させるか取得します．
+     *
+     * @return 表示用の文字
+     */
     public String getChar() {
         return displayChar;
     }
 
+    /**
+     * この Cookie のある位置に移動できるか取得します．
+     * Cookie は true です．
+     *
+     * @return 移動できるかどうかのフラグ
+     */
     public boolean isGo() {
         return goFlag;
     }
 
+    /**
+     * Cookie であることを示すキーを取得します．
+     *
+     * @return 文字列 "c" (private static final String key で指定)
+     */
     public String getKey() {
         return key;
     }
--- src/test/java/io/github/kuri_megane/Objects/CookieTest.java
+++ src/test/java/io/github/kuri_megane/Objects/CookieTest.java
@@ -1,4 +1,27 @@
 package io.github.kuri_megane.Objects;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CookieTest {
+
+    @Test
+    public void testGetChar() {
+        Cookie cookie = new Cookie();
+        assertThat(cookie.getChar(), is("★"));
+    }
+
+    @Test
+    public void testIsGo() {
+        Cookie cookie = new Cookie();
+        assertThat(cookie.isGo(), is(true));
+    }
+
+    @Test
+    public void testGetKey() {
+        Cookie cookie = new Cookie();
+        assertThat(cookie.getKey(), is("c"));
+    }
 }
